8e1328bf26b0830e3c224d8ae11027b6f17354a9
Yixuan_Wang <newbietedsan@gmail.com>
2025-01-30 22:53:49 -0800

    Added simple example code framework for future use

---
+++ Backend/src/main/java/com/quickfolds/backend/user/model/User.java
@@ -0,0 +1,13 @@
+package com.quickfolds.backend.user.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private String name;
+
+}
