6ef191f5584ba86ad45097aa15a71659493769af
Yixuan_Wang <newbietedsan@gmail.com>
2025-02-06 02:17:28 -0800

    Update Origami model

---
+++ Backend/src/main/java/com/quickfolds/backend/community/model/Origami.java
@@ -0,0 +1,56 @@
+package com.quickfolds.backend.community.model;
+
+import com.quickfolds.backend.geometry.model.Step;
+import com.quickfolds.backend.user.model.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import java.time.OffsetDateTime;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "origami")
+public class Origami {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, updatable = false)
+    private Long id;
+
+    @Column(name = "user_id", insertable = false, updatable = false)
+    private Long userId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    @JsonIgnore
+    private User user;
+
+    @Column(name = "is_public", nullable = false)
+    private boolean isPublic;
+
+    @Column(name = "ratings", nullable = false)
+    private int ratings;
+
+    @Column(name = "created_by")
+    private String createdBy;
+
+    @Column(name = "updated_by")
+    private String updatedBy;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private OffsetDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private OffsetDateTime updatedAt;
+
+    // Lazy-loaded one to many fields
+    @OneToMany(mappedBy = "origami", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JsonIgnore
+    private List<Step> steps;
+}
