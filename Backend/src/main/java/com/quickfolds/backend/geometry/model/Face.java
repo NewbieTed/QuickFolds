fd56799c54044b26e880747797f2d0f3aff1ac39
Yixuan_Wang <newbietedsan@gmail.com>
2025-02-06 02:18:03 -0800

    Create Face model

---
+++ Backend/src/main/java/com/quickfolds/backend/geometry/model/Face.java
@@ -0,0 +1,64 @@
+package com.quickfolds.backend.geometry.model;
+
+import com.quickfolds.backend.community.model.Origami;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import java.time.OffsetDateTime;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "face")
+public class Face {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, updatable = false)
+    private Long id;
+
+    @Column(name = "origami_id", insertable = false, updatable = false)
+    private Long origamiId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "origami_id", nullable = false)
+    @JsonIgnore
+    private Origami origami;
+
+    @Column(name = "face_id_in_origami", nullable = false)
+    private int faceIdInOrigami;
+
+    @Column(name = "step_id", insertable = false, updatable = false)
+    private Long stepId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "step_id")
+    @JsonIgnore
+    private Step step;
+
+    @Column(name = "is_deleted", nullable = false)
+    private boolean isDeleted;
+
+    @Column(name = "created_by")
+    private String createdBy;
+
+    @Column(name = "updated_by")
+    private String updatedBy;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private OffsetDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private OffsetDateTime updatedAt;
+
+    // Lazy-loaded one to many fields
+    @OneToMany(mappedBy = "face", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JsonIgnore
+    private List<Vertex> vertices;
+
+}
