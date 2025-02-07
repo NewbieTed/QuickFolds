7df8bfdf60d066eea225fb7e6423d088064d4996
Yixuan_Wang <newbietedsan@gmail.com>
2025-02-07 01:31:12 -0800

    Changed request mapping

--- backend/src/main/java/com/quickfolds/backend/user/controller/UserController.java
+++ backend/src/main/java/com/quickfolds/backend/user/controller/UserController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping("/problem")
+@RequestMapping("/user")
 @RequiredArgsConstructor
 public class UserController {
 
