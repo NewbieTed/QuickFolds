2f637e52d719bf90d2dc782db8900936a9ba0a3a
Yixuan_Wang <newbietedsan@gmail.com>
2025-02-10 00:52:15 -0800

    Added step type constant

---
+++ backend/src/main/java/com/quickfolds/backend/geometry/constants/StepType.java
@@ -0,0 +1,7 @@
+package com.quickfolds.backend.geometry.constants;
+
+public class StepType {
+
+    public static final String FOLD = "fold";
+    public static final String ANNOTATE = "annotate";
+}
