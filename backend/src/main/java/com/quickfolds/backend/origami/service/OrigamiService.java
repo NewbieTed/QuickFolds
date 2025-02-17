e4d8eb164f653544b417294e8cc836902be72e3f
Yixuan_Wang <newbietedsan@gmail.com>
2025-02-17 11:02:56 -0800

    Modified configuration

--- backend/src/main/java/com/quickfolds/backend/origami/service/OrigamiService.java
+++ backend/src/main/java/com/quickfolds/backend/origami/service/OrigamiService.java
@@ -16,7 +16,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
