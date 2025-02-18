bbc088c9724cb38fa2ca5b5ec033b7cbe6a71376
Yixuan_Wang <newbietedsan@gmail.com>
2025-02-17 21:08:22 -0800

    CI test 2

--- backend/src/test/java/com/quickfolds/backend/user/service/UserServiceTest.java
+++ backend/src/test/java/com/quickfolds/backend/user/service/UserServiceTest.java
@@ -10,7 +10,6 @@ import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.*;
