46f6b9af3f2223473f13641bf08a7300d59e1776
bernice <s-btian@lwsd.org>
2025-02-24 11:45:02 -0800

    added annotation response objects

---
+++ backend/src/main/java/com/quickfolds/backend/geometry/model/dto/response/LineAnnotationResponse.java
@@ -0,0 +1,40 @@
+package com.quickfolds.backend.geometry.model.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * DTO (Data Transfer Object) representing the response for a line annotation.
+ * <p>
+ * This class is used to return detailed information about a line annotation in API responses.
+ * It encapsulates data such as the annotated line's identifier in the face and its endpoints.
+ * <p>
+ * Typical use cases include retrieving details of a specific annotated line.
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LineAnnotationResponse {
+
+    /**
+     * The identifier of the annotated line within the face.
+     * <p>
+     * This ID serves to identify the annotated line within a specific face.
+     */
+    private Integer idInFace;
+
+    /**
+     * The ID of the first point in the origami model that defines the line.
+     * <p>
+     * Identifies the first endpoint of the annotated line.
+     */
+    private Integer point1IdInOrigami;
+
+    /**
+     * The ID of the second point in the origami model that defines the line.
+     * <p>
+     * Identifies the second endpoint of the annotated line.
+     */
+    private Integer point2IdInOrigami;
+}
---
+++ backend/src/main/java/com/quickfolds/backend/geometry/model/dto/response/PointAnnotationResponse.java
@@ -0,0 +1,43 @@
+package com.quickfolds.backend.geometry.model.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * DTO (Data Transfer Object) representing the response for a point annotation.
+ * <p>
+ * This class is used to return detailed information about a point annotation in API responses.
+ * It encapsulates data such as the annotated point's identifier in the face and its coordinates.
+ * <p>
+ * Typical use cases include retrieving details of a specific annotated point.
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PointAnnotationResponse {
+
+    /**
+     * The identifier of the annotated point within the face.
+     * <p>
+     * This ID serves to identify the annotated point within a specific face.
+     */
+    private Integer idInFace;
+
+    /**
+     * The x-coordinate of the annotated point.
+     */
+    private Double x;
+
+    /**
+     * The y-coordinate of the annotated point.
+     */
+    private Double y;
+
+    /**
+     * The ID of the edge within the face on which this point is located.
+     * <p>
+     * Indicates which edge the point is on if it's on one.
+     */
+    private Integer onEdgeIdInFace;
+}
