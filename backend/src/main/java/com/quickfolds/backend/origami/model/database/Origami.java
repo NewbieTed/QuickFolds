6bfd75bce6edf5a0151cb6b12826e901308241eb
Yixuan_Wang <newbietedsan@gmail.com>
2025-03-07 22:29:53 -0800

    Added ratings

--- backend/src/main/java/com/quickfolds/backend/origami/model/database/Origami.java
+++ backend/src/main/java/com/quickfolds/backend/origami/model/database/Origami.java
@@ -74,6 +74,14 @@ public class Origami {
     private double ratings;
 
     /**
+     * The number of ratings for the origami model.
+     * <p>
+     * This field stores the number of ratings given to the origami by users.
+     */
+    @Column(name = "num_ratings", nullable = false)
+    private double numRatings;
+
+    /**
      * Identifier of the user who created this origami record.
      * <p>
      * This field may be null if the record was created without explicitly specifying the creator.
